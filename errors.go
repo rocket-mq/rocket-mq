9ba5e6b61e278e5b1f44c3dbd0375d9ce355e9c9
wangben <wangben-1203@qq.com>
2025-01-15 18:33:35 +0800

    receive:判断是否为空消息报的错

--- errors.go
+++ errors.go
@@ -1,5 +1,5 @@
 package rocketmq
 
 func IsMessageNotFoundErr(err error) bool {
-	return err.Error() == "CODE: MESSAGE_NOT_FOUND, MESSAGE: no new message"
+	return err.Error() == "CODE: MESSAGE_NOT_FOUND, MESSAGE: no new message" || err.Error() == "CODE: INTERNAL_SERVER_ERROR, MESSAGE: null. NullPointerException. org.apache.rocketmq.proxy.grpc.v2.consumer.ReceiveMessageActivity.receiveMessage(ReceiveMessageActivity.java:63)"
 }
