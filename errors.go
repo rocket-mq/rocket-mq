40ba93de1bade173c85420b33f41aebc631c3ede
wangben <wangben-1203@qq.com>
2025-01-22 09:01:17 +0800

    优化：消费者拉取不到消息的错误判断

--- errors.go
+++ errors.go
@@ -1,5 +1,16 @@
 package rocketmq
 
+// IsMessageNotFoundErr 是否为消费者拉取不到消息的错误
 func IsMessageNotFoundErr(err error) bool {
-	return err.Error() == "CODE: MESSAGE_NOT_FOUND, MESSAGE: no new message" || err.Error() == "CODE: INTERNAL_SERVER_ERROR, MESSAGE: null. NullPointerException. org.apache.rocketmq.proxy.grpc.v2.consumer.ReceiveMessageActivity.receiveMessage(ReceiveMessageActivity.java:63)"
+	errStr := err.Error()
+	if errStr == "CODE: MESSAGE_NOT_FOUND, MESSAGE: no new message" {
+		return true
+	}
+	if errStr == "CODE: MESSAGE_NOT_FOUND, MESSAGE: no match message" {
+		return true
+	}
+	if errStr == "CODE: INTERNAL_SERVER_ERROR, MESSAGE: null. NullPointerException. org.apache.rocketmq.proxy.grpc.v2.consumer.ReceiveMessageActivity.receiveMessage(ReceiveMessageActivity.java:63)" {
+		return true
+	}
+	return false
 }
