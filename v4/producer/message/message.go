893a16b90d45d117372377ab687e1d2016f634ec
wangben <wangben-1203@qq.com>
2025-02-24 13:32:44 +0800

    v4:顺序消息

--- v4/producer/message/message.go
+++ v4/producer/message/message.go
@@ -41,27 +41,33 @@ func WithProperties(properties map[string]string) Option {
 
 // WithStartDeliverTime sets the delay timestamp of the message
 // 延时消息
+// 毫秒时间戳
 func WithStartDeliverTime(startDeliverTime int64) Option {
 	return func(m *Message) {
 		m.startDeliverTime = startDeliverTime
 	}
 }
 
+// WithDelayTime sets the delay time of the message
+// 延时消息
 func WithDelayTime(t time.Time) Option {
 	return func(m *Message) {
-		ms := t.Sub(time.Now()).Milliseconds()
-		if ms > 0 {
-			m.startDeliverTime = t.Sub(time.Now()).Milliseconds()
-		}
+		m.startDeliverTime = t.UnixMilli()
 	}
 }
 
+// WithTransCheckImmunityTime sets the trans check immunity time of the message
+// 事务消息
+// 第一次消息回查的最快时间,单位秒
 func WithTransCheckImmunityTime(transCheckImmunityTime int) Option {
 	return func(m *Message) {
 		m.transCheckImmunityTime = transCheckImmunityTime
 	}
 }
 
+// WithShardingKey sets the sharding key of the message
+// 顺序消息
+// 分区顺序消息中区分不同分区的关键字段
 func WithShardingKey(shardingKey string) Option {
 	return func(m *Message) {
 		m.shardingKey = shardingKey
